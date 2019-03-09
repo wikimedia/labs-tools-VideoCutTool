6c4c4b66e75d2a4ede295c7299b0bfbbf890e0bc
gopavasanth <gopavasanth1999@gmail.com>
2019-03-10 05:28:47 +0530

    Extended page length

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ class App extends Component {
           <Header>
             <Typography.Title level={2} style={{ color: 'white' }}> VideoCutTool</Typography.Title>
           </Header>
-          <Content style={{ padding: '50px 50px' }}>
+          <Content className='Content' style={{ padding: '50px 50px' }}>
             <Row gutter={16}>
               <Col span={16}>
+                <div className="upolad">
                 <Upload.Dragger>
                   <p className="ant-upload-drag-icon">
                     <Icon type="inbox" />
@@ -23,6 +24,7 @@ class App extends Component {
                   <p className="ant-upload-text">Click or drag file to this area to upload</p>
                   <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
                 </Upload.Dragger>
+                </div>
               </Col>
               <Col span={8}>
                 <h2>Video Trim Settings</h2>
