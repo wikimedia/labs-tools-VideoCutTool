ef32e3e396378e19cdccae159e8bc2d501a63c2d
Sohom <sohomdatta1+git@gmail.com>
2023-05-21 11:11:30 -0400

    Add a much larger time delay compared to before to accomodate docker database spinup times
    Change-Id: I9e4140d813c57810a727692e8f7e2830e39add84

--- server/postgres.js
+++ server/postgres.js
@@ -1,5 +1,13 @@
 const { Sequelize } = require('sequelize');
 const config = require('./config.js');
 
-const sequelize = new Sequelize(config.DB_CONNECTION_URL, { dialect: 'postgres' });
+const sequelize = new Sequelize(
+	config.DB_CONNECTION_URL,
+	{
+		dialect: 'postgres',
+		dialectOptions: {
+			connectTimeout: 10 * 60 * 1000 // Wait 10 minutes before timing out
+		}
+	}
+);
 module.exports = sequelize;
